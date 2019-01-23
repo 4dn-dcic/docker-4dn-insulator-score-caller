ae95c696dbadc54aea04e21474f25f019e7e6dba
Lmercadom <42242797+Lmercadom@users.noreply.github.com>
2019-01-23 17:10:33 -0500

    Update downloads.sh

--- downloads.sh
+++ downloads.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-## SOFTWARE: samtools
-## VERSION: 1.2
-## TYPE: file format converter
-## SOURCE_URL: https://github.com/samtools/samtools
-wget https://github.com/samtools/samtools/releases/download/1.2/samtools-1.2.tar.bz2
-tar -xjf samtools-1.2.tar.bz2
-cd samtools-1.2
-make
-cd ..
-ln -s samtools-1.2 samtools
-
-
 ## SOFTWARE: pairix
 ## VERSION: 0.3.6
 ## TYPE: file format converter,indexer
