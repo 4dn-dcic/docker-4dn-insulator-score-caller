a0ca9cfd3ecc485585f64a91ca74efedbb3742cf
Lmercadom <42242797+Lmercadom@users.noreply.github.com>
2019-02-15 11:09:54 -0500

    Update tests_cwl.sh

--- tests/tests_cwl.sh
+++ tests/tests_cwl.sh
@@ -1,4 +1,3 @@
-set -eE
 CWL_NAME=$1
 CWL_FILENAME=$1.cwl
 INPUT_JSON_NAME=$1.input.json
@@ -8,5 +7,5 @@ LOCAL_CWL_TMPDIR=$CWD/tests/test_tmp
 CWL_RUNNER=$(which cwltool)
 RUNNER_FLAGS="--copy-outputs --no-read-only --no-match-user --outdir $OUTDIR --tmp-outdir-prefix $LOCAL_CWL_TMPDIR --tmpdir-prefix $LOCAL_CWL_TMPDIR --non-strict --debug"
 cd cwl
-$CWL_RUNNER $RUNNER_FLAGS $CWL_FILENAME ../tests/test_input_json/$INPUT_JSON_NAME
+$CWL_RUNNER $RUNNER_FLAGS $CWL_FILENAME ../tests/test_input_json/$INPUT_JSON_NAME || exit
 cd ..
